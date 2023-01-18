470517d4d492ed639581fb1f45cdd905beaccf41
uma08 <umagayathri08@gmail.com>
2023-01-18 15:57:37 +0530

    Update smtp.tf

--- smtp.tf
+++ smtp.tf
@@ -1,7 +1,19 @@
-resource "smtp" "example" {
-  smtp_enable = true
-  smtp_address = "smtp.gmail.com"
-  smtp_port = 587
-  smtp_user_name = "umagayathri08@gmail.com"
-  
+data "googleworkspace_user" "example" {
+  primary_email = "annauma1992@gmail.com"
+}
+
+resource "googleworkspace_user" "alias" {
+  primary_email = "annauma1992@gmail.com"
+  password      = "9840908374"
+  hash_function = "MD5"
+
+  name {
+    family_name = "Annapurna"
+    given_name  = "Gayathri"
+  }
+}
+
+resource "googleworkspace_gmail_send_as_alias" "test" {
+  primary_email = data.googleworkspace_user.example.primary_email
+  send_as_email = googleworkspace_user.alias.primary_email
 }
