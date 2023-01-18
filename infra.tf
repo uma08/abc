df8b061a4ac817c863524f3c170a24d1b2254513
uma08 <umagayathri08@gmail.com>
2023-01-19 02:51:09 +0530

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -27,42 +27,6 @@ provider "google" {
 }
 
 
-# Enable the Compute Engine API
-# Alternatively you can do this directly via the GCP GUI
-resource "google_project_service" "compute" {
-  ## INSERT YOUR PROJECT ID HERE!!
-  project = "my-medium-demo-project-299218"
-  service = "compute.googleapis.com"
-  disable_on_destroy = false
-}
-# Enable the Cloud Resource Manager API
-# Alternatively you can do this directly via the GCP GUI
-resource "google_project_service" "cloudresourcemanager" {
-  ## INSERT YOUR PROJECT ID HERE!!
-  project = "my-medium-demo-project-299218"
-  service = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
 
-# Here we define a very small compute instance
-# The actual content of it isn't important its just here for show-case purposes
-resource "google_compute_instance" "default" {
-  name = "terraform-test-instance"
-  machine_type = "e2-micro"
-  zone = "europe-west1-b"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
 
-  network_interface {
-    network = "default"
-  }
-
-  # Before we can create a compute instance we have to enable the the Compute API
-  depends_on = [
-    google_project_service.cloudresourcemanager,
-    google_project_service.compute]
-}
