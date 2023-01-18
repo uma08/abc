c6f0a4a772a1ffc4052bca26c7ba991879d7ea7e
uma08 <umagayathri08@gmail.com>
2023-01-18 15:36:05 +0530

    Create smtp.tf

---
+++ smtp.tf
@@ -0,0 +1,12 @@
+module "smtp_example" {
+  smtp_enable = true
+  smtp_address = "smtp.gmail.com"
+  smtp_port = 587
+  smtp_user_name = "umagayathri08@gmail.com"
+  smtp_password="my-gmail-password"
+  smtp_domain = "smtp.gmail.com"
+  smtp_authentication = "login"
+  smtp_enable_starttls_auto = true
+  smtp_tls = false
+  smtp_openssl_verify_mode = "peer" 
+}
